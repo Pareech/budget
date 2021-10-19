74bf81345b8e258ad2955760f91537d836b5bb2b
Pareech <zdarma@hotmail.com>
2021-10-18 22:36:25 -0400

    Resubmit changes

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
   <div class='navbar'>
     <div class="dropdown">
-      <button style="color:yellow" class="dropbtn">Deposssits
+      <button style="color:yellow" class="dropbtn">Deposits
         <i class="fa fa-caret-down"></i>
       </button>
       <div class="dropdown-content">
