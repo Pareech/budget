56edeb1a88344180c69803c8240945487792a952
Pareech <zdarma@hotmail.com>
2021-10-18 23:15:28 -0400

    Fix spelling mistake

--- index.php
+++ index.php
@@ -1,44 +1,39 @@
 <!DOCTYPE html>
-<html>
 
-<head>
-  <meta name="viewport" http-equiv="Content-Type" content="text/html, width=device-width, initial-scale=1;" />
-  <link rel='stylesheet' type='text/css' href='css/index.css' />
-  <title>Investments</title>
-</head>
+<meta name="viewport" http-equiv="Content-Type" content="text/html, width=device-width, initial-scale=1;" />
+<link rel='stylesheet' type='text/css' href='css/index.css' />
+
+<title>Investments</title>
+
 
 <!-- Navigation Bar -->
 
-<body>
-
-  <div class='header' ;>
-    <h1>Budget</br>Overview</h1>
-  </div>
 
 
-  <div class='navbar'>
-    <div class="dropdown">
-      <button style="color:yellow" class="dropbtn">Deposits
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-content">
-        <a href=''>Salary</a>
-        <a href=''>Other</a>
+<div class='header' ;>
+  <h1>Budget</br>Overview</h1>
+</div>
 
-      </div>
-    </div>
 
-    <div class="dropdown">
-      <button style="color:yellow" class="dropbtn">Expenses
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-content">
-        <a href=''>Fixed Expenses</a>
-        <a href=''>Misc. Expenses</a>
-        <a href=''>Utilities</a>
-      </div>
+<div class='navbar'>
+  <div class="dropdown">
+    <button style="color:yellow" class="dropbtn">Deposits
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href=''>Salary</a>
+      <a href=''>Other</a>
     </div>
   </div>
-</body>
 
-</html>
+  <div class="dropdown">
+    <button style="color:yellow" class="dropbtn">Expenses
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href=''>Fixed Expenses</a>
+      <a href=''>Misc. Expenses</a>
+      <a href=''>Utilities</a>
+    </div>
+  </div>
+</div>
