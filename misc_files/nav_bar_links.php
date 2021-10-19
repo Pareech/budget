6e6d95ecad4e0aeba00d36f8709a37e47b4e67fb
Pareech <zdarma@hotmail.com>
2021-10-19 00:03:19 -0400

    Navigation Bar Configuration

---
+++ misc_files/nav_bar_links.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<meta name="viewport" http-equiv="Content-Type" content="text/html, width=device-width, initial-scale=1;" />
+<link rel='stylesheet' type='text/css' href='../css/navbar.css' />
+
+<title>Investments</title>
+
+
+<!-- Navigation Bar -->
+
+<div class='navbar'>
+  <div class="dropdown">
+    <button style="color:yellow" class="dropbtn">Depositss
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href='../deposits/salary.php'>Salary</a>
+      <a href=''>Other</a>
+    </div>
+  </div>
+
+  <div class="dropdown">
+    <button style="color:yellow" class="dropbtn">Expenses
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href=''>Fixed Expenses</a>
+      <a href=''>Misc. Expenses</a>
+      <a href=''>Utilities</a>
+    </div>
+  </div>
+  <a style="color:white" ; href='../index.php'>Main Page</a>
+</div>
