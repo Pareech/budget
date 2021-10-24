247eba77a00fc9724ed1809c0d31dfb82daa0d08
Pareech <zdarma@hotmail.com>
2021-10-24 12:59:00 -0400

    Deposit configurations

--- transactions/deposit.php
+++ transactions/deposit.php
@@ -16,57 +16,59 @@ include '../db_connections/connection_pdo.php';
 include '../misc_files/nav_bar_links.php';
 ?>
 
+
 <form name="display" action="" method="POST">
-    <div id="container">
-        <div id="column_left">
-            <table>
-                <tr>
-                    <th colspan="2">Deposit Information</th>
-                </tr>
-                <tr>
-                    <td>Who</td>
-                    <td>
-                        <center><input id="textboxid" name="who" placeholder="Depositor" type="text" /></center>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Amount</td>
-                    <td>
-                        <center><input id="textboxid" name="salary" placeholder="Deposit Amount" type="text" /></center>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Type</td>
-                    <td>
-                        <center><input id="textboxid" name="note" placeholder="eg. Salary" type="text" /></center>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Date</td>
-                    <td>
-                        <center><input type="date" id="textboxid" name="date" /></center>
-                    </td>
-                </tr>
-            </table>
+    <div class="grid-container">
+        <div class="item1a">
+            <h2>Who</h2>
         </div>
-        <div id="column_right">
-            <button type="submit" id="deposit" name="submit_deposit" class="button" value="submit">Submit the<br>Deposit</button>
+        <div class="item2a">
+            <h2>Amount</h2>
         </div>
+        <div class="item3a">
+            <h2>Type</h2>
+        </div>
+        <div class="item4a">
+            <h2>Date</h2>
+        </div>
+        <div class="item1b"><center><input id="textboxid" name="who" placeholder="Depositor" type="text" /></center></div>
+        <div class="item2b"><center><input id="textboxid" name="salary" placeholder="Deposit Amount" type="text" /></center></div>
+        <div class="item3b"><center><input id="textboxid" name="note" placeholder="eg. Salary" type="text" /></center></div>
+        <div class="item4b"><center><input type="date" id="textboxid" name="date" /></center></div>
+        <div class="item5"> <button type="submit" id="transaction_button" name="submit_deposit" class="button" value="submit">Submit<br>Deposit</button></div>
     </div>
 </form>
 
 
 <?php
 if (isset($_POST['submit_deposit'])) {
-    $salary = $_POST['salary'];
-    $depositor = $_POST['who'];
-    $date = $_POST['date'];
-    $note = $_POST['note'];
 
-    $deposit = $pdo->prepare("INSERT INTO income (deposit_amount, depositor, deposit_date, note)
+    $required = array('salary', 'who', 'date', 'note');
+
+    $error = false;
+    foreach ($required as $field) {
+        if (empty($_POST[$field])) {
+            $error = true;
+        }
+    }
+
+    if ($error) {
+        echo
+        "<script>
+            alert('All fields are required.');
+        </script>";
+    } else {
+
+        $salary = $_POST['salary'];
+        $depositor = $_POST['who'];
+        $date = $_POST['date'];
+        $note = $_POST['note'];
+
+        $deposit = $pdo->prepare("INSERT INTO income (deposit_amount, depositor, deposit_date, note)
                           VALUES (:salary_amount, :depositor, :deposit_date, :deposit_type);");
-    $deposit->execute(['salary_amount' => $salary, 'depositor' => $depositor, 'deposit_date' => $date, 'deposit_type' => $note]);
+        $deposit->execute(['salary_amount' => $salary, 'depositor' => $depositor, 'deposit_date' => $date, 'deposit_type' => $note]);
 
-    header("Location: ..");
+        header("Location: ..");
+    }
 }
 ?>
