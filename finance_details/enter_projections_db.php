2ac455ca5db5df9604669630851fe083ad27bff4
Pareech <zdarma@hotmail.com>
2022-02-14 22:41:50 -0500

    Redirect to projections page when DB is updated

--- finance_details/enter_projections_db.php
+++ finance_details/enter_projections_db.php
@@ -7,13 +7,10 @@
 // echo "Due Date: " . $due_date . "<br>";
 // echo "Entry Type: " . $entry_type . "<br>";
 
-$projections = $pdo->prepare("INSERT INTO budget_projection(payee, payment_amount, due_date, transaction_type)
-                                  VALUES (:payee, :payment_amount, :due_date, :entry_type);");
-$projections->execute(['payee' => $payee, 'payment_amount' => $payment_amount, 'due_date' => $due_date, 'entry_type' => $entry_type]);
+$projections = $pdo->prepare("INSERT INTO budget_projection(payee, payment_amount, due_date, transaction_type, payment_method)
+                                  VALUES (:payee, :payment_amount, :due_date, :entry_type, :payment_used);");
+$projections->execute(['payee' => $payee, 'payment_amount' => $payment_amount, 'due_date' => $due_date, 'entry_type' => $entry_type, 'payment_used' => $payment_used]);
 
-// header("Location: ..");
-// echo "Successfully Added <a href='..'>Click Here to Continue</a>";
-
-
+echo "<script> window.location.href='../index.php'</script>";
 
 ?>
