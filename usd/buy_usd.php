34167cfc5ab0a42227adcd2f9a8fd81f3a1014ae
Pareech <zdarma@hotmail.com>
2022-03-24 20:37:55 -0400

    Moved USD heading to seperate page

--- usd/buy_usd.php
+++ usd/buy_usd.php
@@ -16,58 +16,20 @@ $usd_value = $pdo->prepare("SELECT SUM(COALESCE(buy_amt,0)) AS buy_amt,
 ?>
 
 <div class="grid-container">
-    <div class="item1">
-        <table class='table'>
-            <?php
-            $usd_value->execute();
 
-            $money = new NumberFormatter('en', NumberFormatter::CURRENCY);
-            ?>
+    <?php include 'usd_avgs.php'; ?>
 
-            <!-- Get Current USD Values -->
-            <?php
-            $usd_value->execute();
-            foreach ($usd_value as $row) {
-                $buy_amount = $row['buy_amt'];
-                $net_usd = $row['net_usd'];
-                $avg_exch = $row['avg_exch'];
-                $interest = $row['interest'];
-            ?>
-                <tr>
-                    <td id=alnright>CDN Spent</td>
-                    <td id=alnleft><?php echo $money->formatCurrency($buy_amount, 'USD'); ?></td>
-                </tr>
-
-                <tr>
-                    <td id=alnright>Avg Exch. Rate</td>
-                    <td id=alnleft><?php echo $money->formatCurrency($avg_exch, 'USD'); ?></td>
-                </tr>
-
-                <tr>
-                    <td id=alnright>Available USD</td>
-                    <td id=alnleft><?php echo $money->formatCurrency($net_usd, 'USD'); ?></td>
-                </tr>
-
-                <tr>
-                    <td id=alnright>Interest Earned</td>
-                    <td id=alnleft><?php echo $money->formatCurrency($interest, 'USD'); ?></td>
-                </tr>
-            <?php }
-            ?>
-        </table>
-    </div>
     <div class="item2" id="header">
         <h1>Buy US<br>Dollars</h1>
     </div>
 </div>
-
 
 <?php
 include '../misc_files/nav_bar_links.php';
 ?>
 
 <form class="form1" name="display" action="" method="POST">
-    <div class="itemF`">
+    <div class="itemF">
         <h2>Canadian Dollar</h2>
         <input id="textboxid" name="cdn_amount" placeholder="Amount CDN" type="text" />
     </div>
@@ -89,9 +51,9 @@ include '../misc_files/nav_bar_links.php';
 
 <?php
 if (isset($_POST['buy_usd'])) {
+    $date = $_POST['date'];
     $cdn_amount = $_POST['cdn_amount'];
     $exch_rate = $_POST['exch_rate'];
-    $date = $_POST['date'];
 
     $usd_value = round($cdn_amount / $exch_rate, 2);
 
--- usd/sell_usd.php
+++ usd/sell_usd.php
@@ -16,51 +16,13 @@ $usd_value = $pdo->prepare("SELECT SUM(COALESCE(buy_amt,0)) AS buy_amt,
 ?>
 
 <div class="grid-container">
-    <div class="item1">
-        <table class='table'>
-            <?php
-            $usd_value->execute();
 
-            $money = new NumberFormatter('en', NumberFormatter::CURRENCY);
-            ?>
+    <?php include 'usd_avgs.php'; ?>
 
-            <!-- Get Current USD Values -->
-            <?php
-            $usd_value->execute();
-            foreach ($usd_value as $row) {
-                $buy_amount = $row['buy_amt'];
-                $net_usd = $row['net_usd'];
-                $avg_exch = $row['avg_exch'];
-                $interest = $row['interest'];
-            ?>
-                <tr>
-                    <td id=alnright>CDN Spent</td>
-                    <td id=alnleft><?php echo $money->formatCurrency($buy_amount, 'USD'); ?></td>
-                </tr>
-
-                <tr>
-                    <td id=alnright>Avg Exch. Rate</td>
-                    <td id=alnleft><?php echo $money->formatCurrency($avg_exch, 'USD'); ?></td>
-                </tr>
-
-                <tr>
-                    <td id=alnright>Available USD</td>
-                    <td id=alnleft><?php echo $money->formatCurrency($net_usd, 'USD'); ?></td>
-                </tr>
-
-                <tr>
-                    <td id=alnright>Interest Earned</td>
-                    <td id=alnleft><?php echo $money->formatCurrency($interest, 'USD'); ?></td>
-                </tr>
-            <?php }
-            ?>
-        </table>
-    </div>
     <div class="item2" id="header">
         <h1>Sell US<br>Dollars</h1>
     </div>
 </div>
-
 
 <?php
 include '../misc_files/nav_bar_links.php';
