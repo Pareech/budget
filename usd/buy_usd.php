7d80d7bb2421b0879b34c1bc558e7cdb5ab5b31b
Pareech <zdarma@hotmail.com>
2022-04-02 10:00:24 -0400

    Enter USD bought & system calculates exchange rate

--- usd/buy_usd.php
+++ usd/buy_usd.php
@@ -28,23 +28,23 @@ $usd_value = $pdo->prepare("SELECT SUM(COALESCE(buy_amt,0)) AS buy_amt,
 include '../misc_files/nav_bar_links.php';
 ?>
 
-<form class="form1" name="display" action="" method="POST">
-    <div class="itemF">
-        <h2>Canadian Dollar</h2>
-        <input id="textboxid" name="cdn_amount" placeholder="Amount CDN" type="text" />
+<form class="form" name="display" action="" method="POST">
+    <div>
+        <h2>Canadian Amount</h2>
+        <input id="textboxid" name="cdn_amount" placeholder="CDN Amount" type="text" />
     </div>
 
-    <div class="itemF">
-        <h2>Exchange Rate</h2>
-        <input id="textboxid" name="exch_rate" placeholder="Exchange Rate" type="text" />
+    <div>
+        <h2>USD Amount</h2>
+        <input id="textboxid" name="usd_amt" placeholder="USD Amount" type="text" />
     </div>
 
-    <div class="itemF">
+    <div>
         <h2>Date</h2>
         <input type="date" id="textboxid" name="date" />
     </div>
 
-    <div class="itemF">
+    <div class="item">
         <button type="submit" id="transaction_button" name="buy_usd" class="button" value="submit">Buy<br>USD</button>
     </div>
 </form>
@@ -53,13 +53,13 @@ include '../misc_files/nav_bar_links.php';
 if (isset($_POST['buy_usd'])) {
     $date = $_POST['date'];
     $cdn_amount = $_POST['cdn_amount'];
-    $exch_rate = $_POST['exch_rate'];
+    $usd_amt = $_POST['usd_amt'];
 
-    $usd_value = round($cdn_amount / $exch_rate, 2);
+    $exch_rate = round($cdn_amount / $usd_amt, 4);
 
     $buy_usd = $pdo->prepare("INSERT INTO usd_acct(buy_date, buy_amt, exch_rate, usd_value)
                               VALUES (:buy_date, :buy_amt, :exch_rate, :usd_value);");
-    $buy_usd->execute(['buy_date' => $date, 'buy_amt' => $cdn_amount, 'exch_rate' => $exch_rate, 'usd_value' => $usd_value]);
+    $buy_usd->execute(['buy_date' => $date, 'buy_amt' => $cdn_amount, 'exch_rate' => $exch_rate, 'usd_value' => $usd_amt]);
 
     echo
     "<script> 
