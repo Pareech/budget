180232818940b7363851cf762579c6fcca152fbe
Pareech <zdarma@hotmail.com>
2022-04-13 17:59:49 -0400

    Fixed formatting issue

--- finance_details/enter_month_start.php
+++ finance_details/enter_month_start.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 
 <meta name="viewport" http-equiv="Content-Type" content="text/html, width=device-width, initial-scale=1;" />
-<link rel='stylesheet' type='text/css' href='../css/usd_values.css' />
-<title>Sell USD</title>
+<link rel='stylesheet' type='text/css' href='../css/payment_updates.css' />
+<title>Monthly Cash Start Amount</title>
 
 <?php
 include '../db_connections/connection_pdo.php';
 ?>
 
-<div class="grid-container">
-    <div class="item2" id="header">
-        <h1>Enter Monthly<br>Starting Value</h1>
-    </div>
+<div class="header">
+    <h1>Enter Monthly<br>Starting Value</h1>
 </div>
 
 <?php
@@ -19,19 +17,19 @@ include '../misc_files/nav_bar_links.php';
 ?>
 
 <form name="display" action="" method="POST">
-    <div class="grid-container_bottom">
+    <div class="grid-container">
 
-        <div class="item3">
-            <h2>Month Starting Value</h2>
+        <div>
+            <h2>Starting Amount</h2>
             <input id="textboxid" name="start_value" placeholder="Month Starting Value" type="text" />
         </div>
 
-        <div class="item4">
+        <div>
             <h2>Date</h2>
             <input type="date" id="textboxid" name="date" />
         </div>
 
-        <div class="item5a">
+        <div class="itemEMSbutton">
             <button type="submit" id="transaction_button" name="month_start" class="button" value="submit">Add Month<br>Starting Value</button>
         </div>
     </div>
